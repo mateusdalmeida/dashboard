b7e56f167572a2fcef883929c88465a161dce187
mateusdalmeida <mateusdalmeida0@gmail.com>
2020-04-27 17:19:34 -0300

    initial layout edit for generic editor

--- src/components/GenericEditor.vue
+++ src/components/GenericEditor.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="text-center">
-    <v-dialog v-model="isDialogOpen" persistent>
+    <v-dialog v-model="isDialogOpen" persistent max-width="600px">
       <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >Criar/Editar {{$router.currentRoute.name}}</v-card-title>
-
-        <v-container fluid>
-          <div v-for="(fieldType, fieldName) in model" :key="fieldName">
-            <v-text-field
-              :label="fieldName"
-              v-if="fieldType=='string'"
-              v-model="modelAnswers[fieldName]"
-            ></v-text-field>
-            <!-- depois é melhor transformar esse datepicker todo em um componente -->
-            <div v-if="fieldType=='date'">
-              <v-menu
-                v-model="datePickerMenu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
+        <v-card-title class="headline" primary-title>Criar/Editar {{$router.currentRoute.name}}</v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="12"
+                md="4"
+                v-for="(fieldType, fieldName) in model"
+                :key="fieldName"
               >
-                <template v-slot:activator="{ on }">
-                  <v-text-field
-                    readonly
-                    :label="fieldName"
-                    v-on="on"
-                    :value="modelAnswers[fieldName]"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  @input="datePickerMenu = false"
+                <v-text-field
+                  v-if="fieldType=='string'"
                   :label="fieldName"
                   v-model="modelAnswers[fieldName]"
-                ></v-date-picker>
-              </v-menu>
-            </div>
-            <!-- fim do datepicker -->
-            <v-checkbox
-              :label="fieldName"
-              v-if="fieldType=='boolean'"
-              v-model="modelAnswers[fieldName]"
-            ></v-checkbox>
-          </div>
-        </v-container>
+                ></v-text-field>
+
+                <!-- depois é melhor transformar esse datepicker todo em um componente -->
+                <div v-if="fieldType=='date'">
+                  <v-menu
+                    v-model="datePickerMenu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    :top="$vuetify.breakpoint.smAndDown"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        readonly
+                        :label="fieldName"
+                        v-on="on"
+                        :value="modelAnswers[fieldName]"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      @input="datePickerMenu = false"
+                      :label="fieldName"
+                      v-model="modelAnswers[fieldName]"
+                    ></v-date-picker>
+                  </v-menu>
+                </div>
+                <!-- fim do datepicker -->
+                <v-checkbox
+                  v-if="fieldType=='boolean'"
+                  :label="fieldName"
+                  v-model="modelAnswers[fieldName]"
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
           <v-spacer></v-spacer>
