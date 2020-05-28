09b990795edd47e29f52b60501a784c297888a67
Anderson dos Santos Guerra <and.guerra@outlook.com>
2020-05-28 12:11:29 -0300

    Changed pt to ptbr and now tiptap is in portuguese

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import pt from 'vuetify/es5/locale/pt';
+import ptbr from 'vuetify/es5/locale/pt';
 import { project_data } from "@/config/config"
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   lang: {
-    locales: { pt },
-    current: 'pt',
+    locales: { ptbr },
+    current: 'ptbr',
   },
   theme: {
     themes: {
